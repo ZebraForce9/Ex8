85134e68833c9125822c48e1726eba20ea417bc5
ZebraForce9 <58855640+ZebraForce9@users.noreply.github.com>
2020-12-10 22:26:38 +0200

    works for blocks with length greater than one

--- ex8.py
+++ ex8.py
@@ -4,22 +4,31 @@ def constraint_satisfactions(n, blocks):
 
     line = [0 for i in range(n)]
 
-    helper(to_color, line, options, 0)
+    helper(blocks, 0, to_color, line, options, 0)
 
     return options
 
 
-def helper(to_color, line, options, index):
+def helper(blocks, block_index, to_color, line, options, line_index):
     if to_color == 0:
         options.append(line[:])
         return
 
+    if line_index >= len(line) or block_index > len(blocks):
+        return
+
+    block_length = blocks[block_index]
+
     for i in range(len(line)):
-        line[i] = 1
-        to_color -= 1
-        helper(to_color, line, options, index + 2)
-        line[i] = 0
-        to_color += 1
+        if i + block_length > len(line):
+            return
+        line[i: i + block_length] = [1 for i in range(block_length)]
+        to_color -= block_length
+        block_index += 1
+        helper(blocks, block_index, to_color, line, options, line_index + 2)
+        block_index -= 1
+        line[i: i + block_length] = [0 for i in range(block_length)]
+        to_color += block_length
 
 
-print(constraint_satisfactions(3, [1]))
+print(constraint_satisfactions(2, [1]))
