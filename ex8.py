5c6e340bd1a0104d36784c030d0445187ec3e8b9
ZebraForce9 <58855640+ZebraForce9@users.noreply.github.com>
2020-12-11 20:29:29 +0200

    seems to work

--- ex8.py
+++ ex8.py
@@ -4,30 +4,34 @@ def constraint_satisfactions(n, blocks):
 
     line = [0 for i in range(n)]
 
-    helper(blocks, 0, to_color, line, options, 0)
+    constraint_helper(blocks, 0, to_color, line, options, 0)
 
     return options
 
 
-def helper(blocks, block_index, to_color, line, options, line_index):
+def constraint_helper(blocks, block_index, to_color, row, options, line_index):
     if to_color == 0:
-        options.append(line[:])
+        options.append(row[:])
         return
 
-    if line_index >= len(line) or block_index > len(blocks):
+    if line_index >= len(row) or block_index > len(blocks):
         return
 
     block_length = blocks[block_index]
 
-    for i in range(line_index, len(line)):
-        if i + block_length > len(line):
+    for i in range(line_index, len(row)):
+        if i + block_length > len(row):
             return
-        line[i: i + block_length] = [1 for i in range(block_length)]
+        row[i: i + block_length] = [1 for i in range(block_length)]
         to_color -= block_length
         block_index += 1
-        helper(blocks, block_index, to_color, line, options, i + 2)
-        block_index -= 1
-        line[i: i + block_length] = [0 for i in range(block_length)]
-        to_color += block_length
 
-print(constraint_satisfactions(2, [1]))
+        constraint_helper(blocks, block_index, to_color, row, options, i + block_length + 1)
+
+        row[i: i + block_length] = [0 for i in range(block_length)]
+        to_color += block_length
+        block_index -= 1
+
+
+def row_variations(row, blocks):
+    pass
